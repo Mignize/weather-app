449c953eb581c685d2d221baef3f2b4a2f88bc43
Mignize <sanolaxd@hotmail.com>
2022-03-08 17:03:48 -0500

    feat: navigation and create settings

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
-import './App.css';
+import React from "react";
+import "./App.css";
+import Navigation from "./components/Navigation.js";
 
 function App() {
+  const changeBackground = () => {
+    const body = document.body;
+    const date = new Date();
+    const hour = date.getHours();
+    if (hour >= 18) {
+      body.setAttribute("class", "night");
+    } else if (hour >= 12) {
+      body.setAttribute("class", "sunset");
+    } else if (hour >= 0) {
+      body.setAttribute("class", "sky");
+    }
+  };
+  changeBackground();
   return (
-    <div className="App">
-    </div>
+    <>
+      <Navigation></Navigation>
+    </>
   );
 }
 
 export default App;
+
---
+++ src/settings.js
@@ -0,0 +1,8 @@
+import 'dotenv/config';
+
+const settings = {
+    baseUrl: 'http://api.weatherapi.com/v1',
+    key: process.env.KEY,
+}
+
+export default settings;
